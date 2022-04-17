c1e3eaf6eea45bd9239e606c1cbb991df7b8e40d
Melissa <melissainui@gmail.com>
2022-04-17 15:58:41 -0300

    add values to table filme

---
+++ schema/populate/filme.sql
@@ -0,0 +1,21 @@
+INSERT INTO FILME (eh_sequencia, classif_etaria) VALUES
+(TRUE, 'L'), -- Harry Potter e a Pedra Filosofal
+(TRUE, 'L'), -- Harry Potter e a Camara Secreta
+(TRUE, '10'), -- Harry Potter e o Prisioneiro de Azkaban
+(TRUE, 'L'), -- Harry Potter e o Calice de Fogo
+(TRUE, '12'), -- O Senhor dos Aneis: O Retorno do Rei
+(TRUE, '12'), -- O Senhor dos Aneis: As Duas Torres
+(TRUE, '12'), -- O Senhor dos Aneis: A Sociedade do Anel
+(TRUE, '16'), -- O Hobbit: Uma Jornada Inesperada
+(TRUE, '12'), -- O Hobbit: A Desolação de Smaug
+(TRUE, '16'), -- O Hobbit: A Batalha dos Cinco Exercitos
+(FALSE, 'L'), -- Red: Crescer é uma Fera
+(FALSE, 'L'), -- Encanto
+(FALSE, '14'), -- Sr e Sra Smith
+(TRUE, 'L'), -- A Lenda do Tesouro Perdido
+(FALSE, '10'), -- O Rei do Show
+(TRUE, '12'), -- Homem-Aranha: De Volta ao Lar
+(TRUE, '12'), -- Homem-Aranha: Longe de Casa
+(FALSE, '10'), -- Extraordinário
+(TRUE, '12'), -- Duro de Matar: A Vingança
+(FALSE, 'L'); -- A Procura da Felicidade
