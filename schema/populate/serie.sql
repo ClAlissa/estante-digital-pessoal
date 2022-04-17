c450ebd35542a21f91a67c475888f29add23db59
Melissa <melissainui@gmail.com>
2022-04-17 19:32:44 -0300

    add values to serie table

---
+++ schema/populate/serie.sql
@@ -0,0 +1,20 @@
+INSERT INTO SERIE (data_final) VALUES
+('2013-09-29'), -- Breaking Bad 
+('2008-07-19'), -- Avatar: A Lenda de Aang
+(NULL), -- Rick e Morty
+(NULL), -- Arcane
+('2020-10-23'), -- O Gambito da Rainha
+(NULL), -- Fargo 
+('2019-06-05'), -- Black Mirror
+('2020-06-27'), -- Dark 
+(NULL), -- Stranger Things
+('2020-01-31'), -- BoJack Horseman
+('2019-11-24'), -- Anne With an "E" 
+(NULL), -- The Crown
+('2020-10-09'), -- A Maldição da Residência Hill
+('2020-12-30'), -- Vikings 
+('2019-09-25'), -- Suits 
+('2019-01-27'), -- Outlander 
+('2021-09-16'), -- Brooklyn Nine-Nine
+(NULL), -- The Witcher
+('2021-12-03'); -- La casa de papel
