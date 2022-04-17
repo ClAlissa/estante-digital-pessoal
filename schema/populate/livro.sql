edc0f512595817b59a3e5badbc8801eb38583263
Melissa <melissainui@gmail.com>
2022-04-17 18:41:49 -0300

    add values to livro table

---
+++ schema/populate/livro.sql
@@ -0,0 +1,25 @@
+INSERT INTO LIVRO (editora, num_edicao, eh_saga) VALUES
+(' Estação Liberdade', 1, FALSE), -- Don Juan
+('Companhia das Letras', 2, FALSE), -- Memorial do Convento
+('Nova Fronteira', 1, FALSE), -- O inominável
+('Rocco', 1, FALSE), -- Mulheres que correm com os lobos
+('Seguinte', 1, FALSE), -- Vermelho, branco e sangue azul
+('Companhia das Letras', 1, FALSE), -- A revolução dos bichos
+('Companhia das Letras', 1, FALSE), -- 1984
+('Intrínseca', 1, TRUE), -- Como Eu Era Antes de Você 
+('Martin Claret', 1, FALSE), -- Orgulho e Preconceito
+('Harper Collins', 1, FALSE), -- O Pequeno Príncipe
+('Geração Editorial', 1, FALSE), -- O Pequeno Príncipe
+('Clássicos Zahar', 1, FALSE), -- Um Estudo em Vermelho
+('Darkside', 1, FALSE), -- O Retrato de Dorian Gray
+('Darkside', 1, FALSE), -- O Colecionador
+('Seguinte', 1, TRUE), -- A Seleção
+('Galera', 43, TRUE), -- Corte de Espinhos e Rosas
+('Clássicos Zahar', 1, FALSE), -- O Morro dos Ventos Uivantes
+('Editora Aleph', 1, TRUE), -- Messias de Duna
+('Editora Aleph', 2, TRUE), -- Filhos de Duna
+('Intrínseca', 1, TRUE), -- O Homem de Giz
+('Intrínseca', 1, TRUE), -- O que aconteceu com Annie
+('Intrínseca', 1, TRUE), -- Outras pessoas
+('Faro Editorial', 1, FALSE), -- A Garota do Lago
+('Companhia das Letrinhas', 1, FALSE); -- A Parte Que Falta
