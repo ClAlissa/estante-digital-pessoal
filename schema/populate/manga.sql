5dc91cc8dde5d5bd104990b6914940ae42281f59
Melissa <melissainui@gmail.com>
2022-04-17 20:16:17 -0300

    add values to table

---
+++ schema/populate/manga.sql
@@ -0,0 +1,20 @@
+INSERT INTO MANGA (tem_adapt, nome_estudio) VALUES
+(TRUE, 'Bones'), -- Fullmetal Alchemist
+(TRUE, 'A.P.P.P.'), -- JoJo's Bizarre Adventure
+(TRUE, 'Wit Studio'), -- Attack on Titan
+(TRUE, 'A-1 Pictures'), -- ERASED
+(TRUE, 'Ufotable'), -- Demon Slayer
+(TRUE, 'Bones'), -- Boku No Hero Academia
+(TRUE, 'Toei Animation'), -- One Piece
+(TRUE, 'Telecom Animation Film'), -- Orange
+(TRUE, 'Production I.G'), -- Haikyuu!!
+(TRUE, 'MAPPA'), -- Jujutsu Kaisen
+(TRUE, 'A-1 Pictures'), -- Fairy Tail
+(TRUE, 'MAPPA'), -- Banana Fish
+(TRUE, 'Madhouse'), -- One Punch-Man
+(TRUE, 'Hibari'), -- Given
+(TRUE, ' Kyoto Animation'), -- A Forma da Voz
+(TRUE, 'Nippon Animation'), -- Hunter X Hunter
+(FALSE, 'Redice'), -- Solo Leveling
+(TRUE, 'Lerche'), -- Assassination Classroom
+(TRUE, 'Kyoto Animation'); -- Violet Evergarden
