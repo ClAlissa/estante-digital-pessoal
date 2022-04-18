1f795f560c745bbd94cea2a9b791cb0bbe78fd1e
ClAlissa <91644764+ClAlissa@users.noreply.github.com>
2022-04-18 15:53:09 -0300

    Update personagem.sql

---
+++ schema/populate/personagem.sql
@@ -0,0 +1,23 @@
+INSERT INTO personagem(nome_pers, id_midia, id_ator, descricao) VALUES
+('Bilbo Baggins', 8, NULL, 'idoso com seus quase 111 anos, escreve seu livro chamado "Lá e de Volta Outra Vez" que narra sua aventura quando jovem'), -- O Hobbit: Uma Jornada Inesperada
+('Mei Lee', 11, NULL, 'uma garota sino-canadense de 13 anos de idade, que está dividida entre continuar sendo a filha zelosa de sua mãe e o caos da adolescência'), -- Red: Crescer é uma Fera
+('Peter Parker', 16, NULL,'um jovem que começa a navegar em sua recém-descoberta identidade como o super-herói disparador de teia'), -- Homem-Aranha: De Volta ao Lar
+('August Matthew Pullman', 18, NULL, 'um garoto de 10 anos que nasceu com uma rara deformidade facial médica e passou por 27 cirurgias diferentes para ver, cheirar, falar e ouvir sem um aparelho auditivo'), -- Extraordinário
+('Chris Gardner', 20, NULL, 'um pai de família que enfrenta problemas financeiros e sua mulher o deixa com ele o filho Christopher, de cinco anos'), -- A Procura da Felicidade (Ator)
+
+('Don Juan', 21, NULL, 'um jovem conquistador que trabalha em conquistar uma mulher de alma pura por conta do desafio proposto por seu amigo'), -- Don Juan
+('Winston Smith', 27, NULL, 'um homem com uma vida aparentemente insignificante, que recebe a tarefa de perpetuar a propaganda do regime através da falsificação de documentos públicos'), -- 1984
+('Dorian Gray', 33, NULL, 'um belo rapaz, quem concordou em posar para o mais novo quadro de Henry'), -- O Retrato de Dorian Gray
+('Kelsey Castle', 43, NULL, 'uma jornalista investigativa designada para mostrar ao mundo a verdade sobre o caso da garota'), -- A Garota do Lago
+
+('Aang', 46, NULL, 'um garoto que é um dominador de ar (poderes do mundo fictício), além de ser o Avatar que sumiu há cem anos atrás'), -- Avatar: A Lenda de Aang
+('Lorne Malvo', 50, NULL, 'um homem, com sua malícia e violência, começa a influenciar a população da cidade Bemidji'), -- Fargo 
+('Anne Shirley', 55, NULL, 'uma garota órfã de 13 anos, imaginativa, brilhante, animada e falante'), -- Anne With an "E" 
+('Ragnar Lodbrok', 58, NULL, 'um fazendeiro que alcança a fama por seus ataques bem-sucedidos na Inglaterra e, mais tarde, se torna um Rei'), -- Vikings 
+('Geralt de Rivia', 62, NULL, 'um solitário caçador de monstros, que luta para encontrar seu lugar num mundo onde pessoas são mais cruéis que criaturas'),-- The Witcher
+
+('Edward Elric', 64, NULL, 'um garoto loiro com dois membros de metal, eventualmente se torna alquimista com certificado nacional'), -- Fullmetal Alchemist
+('Satoru Fujinuma', 67, NULL, ' um homem que foi mandado para 18 anos atrás para evitar os eventos que levaram à morte de sua mãe'), -- ERASED
+('Tanjiro Kamado', 68, NULL, 'um garoto que teve sua família massacrada e sua irmã amaldiçoada pelos demônios. Inicia sua jornada para encontrar a cura e se vingar'), -- Demon Slayer
+('Yuuji Itadori', 73, NULL, 'Um adolescente musculoso com grandes olhos castanhos claros e cabelos castanhos espetados'), -- Jujutsu Kaisen
+('Shōya Ishida', 78, NULL, 'foi a principal pessoa que praticou bullying com Shouko, tendo levado a culpa sozinho pela ação de todos os seus amigos'); -- A Forma da Voz
