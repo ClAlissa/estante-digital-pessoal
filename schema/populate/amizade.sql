8b98103644d3f886fd13b81144544974dc8d5f27
ClAlissa <91644764+ClAlissa@users.noreply.github.com>
2022-04-18 01:37:12 -0300

    Update amizade.sql

---
+++ schema/populate/amizade.sql
@@ -0,0 +1,14 @@
+INSERT INTO amizade(nome_usuario_env, nome_usuario_rec, data_inicio) VALUES
+
+('Rosanna', 'Dominick', '2020-10-23'),
+('Dominick', 'Rosanna', '2020-10-23'),
+
+('Filip', 'Francisco', '2021-12-25'),
+('Francisco', 'Filip', '2021-12-26'),
+
+('Eileen', 'Aveline',  '2021-02-14'),
+('Aveline', 'Eileen', '2021-02-14'),
+('Evelin', 'Aveline', '2021-02-14'),
+( 'Aveline', 'Evelin', '2021-02-14'),
+('Evelin', 'Eileen', '2021-02-14'),
+('Eileen', 'Evelin', '2021-02-14');
