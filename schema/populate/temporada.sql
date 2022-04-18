d7e803301421960a6a77ca43f3acbd4f83dfa875
ClAlissa <91644764+ClAlissa@users.noreply.github.com>
2022-04-18 00:43:01 -0300

    Update temporada.sql
    adicionado todas temporadas de todas midias

--- schema/populate/temporada.sql
+++ schema/populate/temporada.sql
@@ -1,3 +1,203 @@
-INSERT INTO temporada(numero_temp, id_midia) VALUES (1, 41);
+INSERT INTO temporada(numero_temp, id_midia) VALUES (1, 41); -- O que aconteceu com Annie
 INSERT INTO temporada(numero_temp, id_midia) VALUES (2, 41);
 INSERT INTO temporada(numero_temp, id_midia) VALUES (3, 41);
+
+INSERT INTO temporada(numero_temp, id_midia) VALUES
+(1, 21), -- Don Juan
+(1, 22), -- Memorial do Convento
+(1, 23), -- O inominável
+(1, 24), -- Mulheres que correm com os lobos
+(1, 25), -- Vermelho, branco e sangue azul
+(1, 26), -- A revolução dos bichos
+(1, 27), -- 1984
+(1, 29), -- Orgulho e Preconceito
+(1, 30), -- O Pequeno Príncipe
+(1, 31), -- O Pequeno Príncipe
+(1, 32), -- Um Estudo em Vermelho
+(1, 33), -- O Retrato de Dorian Gray
+(1, 34), -- O Colecionador
+(1, 37), -- O Morro dos Ventos Uivantes
+(1, 42), -- Outras pessoas
+(1, 43), -- A Garota do Lago
+(1, 44), -- A Prte Que Falta
+
+(1, 48), -- Arcane
+(1, 49), -- O Gambito da Rainha
+(1, 57), -- A Maldição da Residência Hill
+
+
+(1, 67), -- ERASED
+(1, 71), -- Orange
+(1, 73), -- Jujutsu Kaisen
+(1, 75), -- Banana Fish
+(1, 77), -- Given
+(1, 78), -- A Forma da Voz
+(1, 80), -- Solo Leveling
+(1, 82); -- Violet Evergarden
+
+INSERT INTO temporada(numero_temp, id_midia) VALUES
+(1, 1), -- Como Eu Era Antes de Você
+(2, 1),
+(3, 1),
+(1, 35), -- A Seleção
+(2, 35),
+(3, 35),
+(4, 35),
+(5, 35),
+(6, 35),
+(1, 36), -- Corte de Espinhos e Rosas
+(2, 36),
+(3, 36),
+(4, 36),
+(2, 38), -- Messias de Duna
+(3, 39), -- Filhos de Duna
+(1, 40), -- O Homem de Giz 
+(2, 40),
+(3, 40),
+
+(1, 45), -- Breaking Bad
+(2, 45),
+(3, 45),
+(4, 45),
+(5, 45),
+(1, 46), -- Avatar: A Lenda de Aang
+(2, 46),
+(3, 46),
+(1, 47), -- Rick e Morty
+(2, 47),
+(3, 47),
+(4, 47),
+(5, 47),
+(1, 50), -- Fargo
+(2, 50),
+(3, 50),
+(4, 50),
+(1, 51), -- Black Mirror
+(2, 51),
+(3, 51),
+(4, 51),
+(5, 51),
+(1, 52), -- Dark
+(2, 52),
+(3, 52),
+(1, 53), -- Stranger Things
+(2, 53),
+(3, 53),
+(4, 53),
+(1, 54), -- BoJack Horseman
+(2, 54),
+(3, 54),
+(4, 54),
+(5, 54),
+(6, 54),
+(1, 55), -- Anne With an "E"
+(2, 55),
+(3, 55),
+(1, 56), -- The Crown
+(2, 56),
+(3, 56),
+(4, 56),
+(1, 58), -- Vikings
+(2, 58),
+(3, 58),
+(4, 58),
+(5, 58),
+(6, 58),
+(1, 59), -- Suits
+(2, 59),
+(3, 59),
+(4, 59),
+(5, 59),
+(6, 59),
+(7, 59),
+(8, 59),
+(9, 59),
+(1, 60), -- Outlander
+(2, 60),
+(3, 60),
+(4, 60),
+(5, 60),
+(1, 61), -- Brooklyn Nine-Nine
+(2, 61),
+(3, 61),
+(4, 61),
+(5, 61),
+(6, 61),
+(7, 61),
+(8, 61),
+(1, 62), -- The Witcher
+(2, 62),
+(1, 63), -- La casa de papel
+(2, 63),
+(3, 63),
+(4, 63),
+(5, 63),
+
+(1, 64), -- Fullmetal Alchemist
+(2, 64),
+(1, 65), -- JoJo's Bizarre Adventure
+(2, 65),
+(3, 65),
+(4, 65),
+(5, 65),
+(6, 65),
+(1, 66), -- Attack on Titan
+(2, 66),
+(3, 66),
+(4, 66),
+(5, 66),
+(1, 68), -- Demon Slayer
+(2, 68),
+(1, 69), -- Boku No Hero Academia
+(2, 69),
+(3, 69),
+(4, 69),
+(5, 69),
+(1, 70), -- One Piece
+(2, 70),
+(3, 70),
+(4, 70),
+(5, 70),
+(6, 70),
+(7, 70),
+(8, 70),
+(9, 70),
+(10, 70),
+(11, 70),
+(12, 70),
+(13, 70),
+(14, 70),
+(15, 70),
+(16, 70),
+(17, 70),
+(18, 70),
+(19, 70),
+(20, 70),
+(21, 70),
+(22, 70),
+(23, 70),
+(1, 72), -- Haikyuu!!
+(2, 72),
+(3, 72),
+(4, 72),
+(1, 74), -- Fairy Tail
+(2, 74), 
+(3, 74), 
+(4, 74), 
+(5, 74), 
+(6, 74), 
+(7, 74), 
+(8, 74), 
+(9, 74), 
+(1, 76), -- One Punch-Man
+(2, 76),
+(1, 79), -- Hunter X Hunter
+(2, 79), 
+(3, 79), 
+(4, 79), 
+(5, 79), 
+(6, 79), 
+(7, 79), 
+(1, 81), -- Assassination Classroom
+(2, 81),
+(3, 81);
