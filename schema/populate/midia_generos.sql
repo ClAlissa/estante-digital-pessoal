5147138e0687a8173dafd2ae0fc7ead182fbf1d8
ClAlissa <91644764+ClAlissa@users.noreply.github.com>
2022-04-18 23:42:56 -0300

    Update midia_generos.sql

---
+++ schema/populate/midia_generos.sql
@@ -0,0 +1,304 @@
+INSERT INTO midia_generos(genero, id_midia) VALUES
+('Aventura', 1), -- Harry Potter e a Pedra Filosofal 
+('Fantasia', 1),
+('Infantil', 1),
+('Ficção', 1),
+('Aventura', 2), -- Harry Potter e a Camara Secreta
+('Fantasia', 2),
+('Infantil', 2),
+('Mistério', 2),
+('Aventura', 3), -- Harry Potter e o Prisioneiro de Azkaban
+('Fantasia ', 3),
+('Infantil ', 3),
+('Mistério', 3),
+('Filme familiar', 3),
+('Ficção', 3),
+('Aventura', 4), -- Harry Potter e o Calice de Fogo
+('Fantasia', 4),
+('Infantil', 4),
+('Mistério', 4),
+('Suspense', 4),
+('Ação', 5), -- O Senhor dos Aneis: O Retorno do Rei
+('Fantasia', 5),
+('Aventura', 5),
+('Drama', 5),
+('Ação', 6), -- O Senhor dos Aneis: As Duas Torres
+('Fantasia', 6),
+('Aventura', 6),
+('Drama', 6),
+('Ação', 7), -- O Senhor dos Aneis: A Sociedade do Anel
+('Fantasia', 7), 
+('Aventura', 7), 
+('Ação', 8), -- O Hobbit: Uma Jornada Inesperada
+('Fantasia', 8),
+('Aventura', 8),
+('Ficção científica', 8),
+('Infantil', 8),
+('Alta fantasia', 8),
+('Aventura', 9), -- O Hobbit: A Desolação de Smaug
+('Alta fantasia', 9),
+('Fantasia', 9),
+('Drama', 9),
+('Aventura', 10), -- O Hobbit: A Batalha dos Cinco Exercitos
+('Alta fantasia', 10), 
+('Fantasia', 10), 
+('Infantil', 11), -- Red: Crescer é uma Fera
+('Comédia', 11),
+('Filme familiar', 11),
+('Amadurecimento', 11),
+('Animação', 11),
+('Fantasia', 11),
+('Infantil', 12), -- Encanto
+('Fantasia', 12),
+('Filme musica', 12),
+('Filme familiar', 12),
+('Comédia', 12),
+('Animação', 12),
+('Aventura', 12),
+('Desenho animado', 12),
+('Ação', 13), -- Sr e Sra Smith
+('Comédia', 13), 
+('Aventura', 13), 
+('Romance', 13), 
+('Suspense', 13), 
+('Comédia romântica', 13), 
+('Crime', 13), 
+('Humor ácido', 13), 
+('Aventura', 14), -- A Lenda do Tesouro Perdido
+('Ação', 14),
+('Mistério', 14),
+('Suspense', 14),
+('Filme musical', 15), -- O Rei do Show
+('Romance', 15),
+('Drama', 15),
+('Ação', 16), -- Homem-Aranha: De Volta ao Lar
+('Aventura', 16),
+('Filme de super-herói', 16),
+('Ficção científica', 16),
+('Fantasia', 16),
+('Adolecente', 16),
+('Ação', 17), -- Homem-Aranha: Longe de Casa
+('Comédia', 17),
+('Filme de super-herói', 17),
+('Ficção científica', 17),
+('Adolecente', 17),
+('Infantil', 18), -- Extraordinário
+('Comédia', 18),
+('Drama', 18),
+('Aventura', 19), -- Duro de Matar: A Vingança
+('Ação', 19),
+('Suspense', 19),
+('Ficção policials', 19),
+('Drama', 20), -- A Procura da Felicidade
+
+
+('Ficção literária', 21), -- Don Juan
+('Romance', 22), -- Memorial do Convento
+('Ficção histórica', 22),
+('Romance de amor', 22),
+('Romance', 23), -- O inominável
+('Ficção', 23), 
+('Ficção', 24), -- Mulheres que correm com os lobos
+('Romance de amor', 25), -- Vermelho, branco e sangue azul
+('Ficção humorística', 25),
+('Literatura LGBT', 25),
+('Alegoria', 26), -- A revolução dos bichos
+('Sátira', 26),
+('Fábula', 26),
+('Novela', 26),
+('Sátira', 26),
+('Roman à clef', 26),
+('Ficção distópica', 26),
+('Ficção científica', 27), -- 1984
+('Ficção política', 27),
+('Ficção científica social', 27),
+('Ficção distópica', 27),
+('Romance de amor', 28), -- Como Eu Era Antes de Você
+('Ficção', 28),
+('Romance de amor', 29), -- Orgulho e Preconceito
+('Ficção', 29),
+('Sátira', 29),
+('Romance de costume', 29),
+('Literatura infantil', 30), -- O Pequeno Príncipe
+('Fábula', 30),
+('Novela', 30),
+('Ficção especulativa', 30),
+('Literatura infantil', 31), -- O Pequeno Príncipe
+('Fábula', 31),
+('Novela', 31),
+('Ficção especulativa', 31),
+('História de detetives', 32), -- Um Estudo em Vermelho
+('Romance', 32),
+('Ficção policial', 32),
+('Mistério', 32),
+('Ficção gótica', 33), -- O Retrato de Dorian Gray
+('Romance filosófico', 33),
+('Romance', 34), -- O Colecionador
+('Suspense', 34),
+('Terror', 34),
+('Romance psicológico', 34),
+('Ficção juvenil', 35), -- A Seleção
+('Romance', 35),
+('Literatura fantástica', 35),
+('Ficção distópica', 35),
+('Romance', 36), -- Corte de Espinhos e Rosas
+('Ficção juvenil', 36),
+('Literatura fantástica', 36),
+('Alta fantasias', 36),
+('Romance', 37), -- O Morro dos Ventos Uivantes
+('Romance de amor', 37),
+('Ficção gótica', 37),
+('Tragédia', 37),
+('Romance', 38), -- Messias de Duna
+('Ficção científica', 38),
+('Romance', 39), -- Filhos de Duna
+('Ficção científica', 39),
+('Romance', 40), -- O Homem de Giz
+('Suspense', 40),
+('Mistério', 40),
+('Romance psicológico', 40),
+('Suspense', 41), -- O que aconteceu com Annie
+('Suspense', 42), -- Outras pessoas -  
+('Mistério', 42),
+('Romance psicológico', 42),
+('Suspense psicológico', 42),
+('Suspense', 43), -- A Garota do Lago 
+('Mistério', 43),
+('Ficção', 43),
+('Suspense psicológico', 43),
+('Policial processual', 43),
+('Literatura infantil', 44), -- A Parte Que Falta
+('Humor ácido', 45), -- Breaking Bad ('Drama', 45),
+('Crime', 45),
+('Suspense', 45),
+('Tragédia', 45),
+('Crime TV genre', 45),
+('Animação', 46), -- Avatar: A Lenda de Aang
+('Comédia', 46),
+('Infantil', 46),
+('Aventura', 46),
+('Steampunk', 46),
+('Fantasia televisiva', 46),
+('Ação', 46),
+('Humor ácido', 47), -- Rick e Morty  
+('Sitcom', 47),
+('Comédia', 47),
+('Aventura', 47),
+('Ficção de aventura', 47),
+('Steampunk', 48), -- Arcane
+('Ficção de aventura', 48),
+('Ação', 48),
+('Fantasia científica', 48),
+('Filme histórico', 49), -- O Gambito da Rainha
+('Amadurecimento', 49),
+('Crime', 50), -- Fargo
+('Humor ácido', 50),
+('Suspense', 50),
+('Drama', 50),
+('Distopia', 51), -- Black Mirror
+('Ficção cinetífica', 51),
+('Suspense', 51),
+('Drama', 51),
+('Sátira', 51),
+('Ficção especulativa', 51),
+('Sobrenatural', 52), -- Dark
+('Suspense', 52),
+('Drama', 52),
+('Mistério', 52),
+('Suspense psicológico', 52),
+('Tragédia', 52),
+('Suspense', 53), -- Stranger Things
+('Terror', 53),
+('Sobrenatural', 53),
+('Drama', 53),
+('Obra de Época', 53),
+('Animação', 54), -- BoJack Horseman
+('Comédia', 54),
+('Humor ácido', 54),
+('Drama', 54),
+('Sátira', 54),
+('Comédia dramática', 54),
+('Humor absurdo', 54),
+('Tragicomédia', 54),
+('Obra de Época', 55), -- Anne With an "E"
+('Drama', 56), -- The Crown
+('Obra de Época', 56),
+('História', 56),
+('Terror', 57), -- A Maldição da Residência Hill - 
+('Drama', 57),
+('Ficção supernatural', 57),
+('Drama', 58), -- Vikings
+('Guerra', 58),
+('Obra de Época', 58),
+('Aventura', 58),
+('Ficção histórica', 58),
+('Ficção de aventura', 58),
+('Ação', 58),
+('Drama', 59), -- Suits
+('Drama jurídico', 59),
+('Comédia', 59),
+('Ficção histórica', 60), -- Outlander
+('Romance', 60),
+('Ficção científica', 60),
+('Drama', 60),
+('Aventura', 60),
+('Fantasias', 60),
+('Sitcom', 61), -- Brooklyn Nine-Nine
+('Comédia', 61),
+('Policial processual', 61),
+('Aventura', 62), -- The Witcher
+('Ação', 62),
+('Fantasia', 62),
+('Drama', 62),
+('Ficção de aventura', 62),
+('Drama', 63), -- La casa de papel
+('Suspense', 63),
+('Crime', 63),
+('Comédia dramática', 63),
+
+
+('Ficção científica', 64), -- Fullmetal Alchemist
+('Aventura', 64),
+('Ação', 65), -- JoJo's Bizarre Adventure
+('Aventura', 65),
+('Ficção pós-apocalíptica', 66), -- Attack on Titan
+('Ação', 66),
+('Drama', 66),
+('Suspense', 67), -- ERASED
+('Drama', 67),
+('Mistério', 67),
+('Ficção de aventura', 68), -- Demon Slayer
+('Artes marciais', 68),
+('Ficção de aventura', 69), -- Boku No Hero Academia
+('História de super-herói', 69),
+('Ação', 70), -- One Piece
+('Aventura', 70),
+('Comédia', 70),
+('Romance', 71), -- Orange
+('Adolescente', 71),
+('Comédia', 72), -- Haikyuu!!
+('Mangá de esporte', 72),
+('Ficção supernatural', 73), -- Jujutsu Kaisen
+('Ficção de aventura', 73),
+('Ação', 74), -- Fairy Tail
+('Aventura', 74),
+('Fantasia', 74),
+('Crime', 75), -- Banana Fish
+('Suspense', 75),
+('Ação', 76), -- One Punch-Man
+('Comédia', 76),
+('História de super-herói', 76),
+('Drama', 77), -- Given
+('Romance de amor', 77),
+('Drama', 78), -- A Forma da Voz
+('Romance', 78),
+('Slice of Life', 78),
+('Ação', 79), -- Hunter X Hunter
+('Aventura', 79),
+('Isekai', 80), -- Solo Leveling
+('Ação', 81), -- Assassination Classroom
+('Comédia', 81),
+('Ficção científica', 81),
+('Drama', 82), -- Violet Evergarden
+('Fantasia', 82),
+('Romance', 82);
